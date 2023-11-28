3ce483b324ce66da86ec15ae6c76c55db99e5eeb
ayuure <bertinaayuure0147@gmail.com>
2023-11-28 17:39:10 +0000

    fixed

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(97) < c and  c < ord(123):
+        return True
+    return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        val = ord(str[i])
+        if 97 <= val < 123:
+            val = val - 32
+        print("{:c}".format(val), end ='')
