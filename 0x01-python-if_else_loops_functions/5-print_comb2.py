4f3886297d52bfbd1649b2a174e1ebf643004b9b
ayuure <bertinaayuure0147@gmail.com>
2023-11-28 11:08:09 -0800

    fixed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        print(f"{i}{j}", end=' ')
-        print()
+        print("{:d}{:d}".format(i, j), end=', ' if i < 9 or j < 9 else "\n")
