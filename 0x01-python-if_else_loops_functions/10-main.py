d7f4cec473d5229fa60b9f77e2971335705c82c9
ayuure <bertinaayuure0147@gmail.com>
2023-11-29 08:31:14 +0000

    update

--- 0x01-python-if_else_loops_functions/10-main.py
+++ 0x01-python-if_else_loops_functions/10-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-pow = __import__('10-add').pow
+add = __import__('10-add').add
 
-print(pow(1, 2))
-print(pow(98, 0))
-print(pow(100, -2))
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
