7b1d424727a473fc41f2121191e38acf0c0d1025
ayuure <bertinaayuure0147@gmail.com>
2023-11-29 04:05:31 -0800

    fixed style issues

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,6 @@
-
 #!/usr/bin/python3
 def remove_char_at(str, n):
-        if str == 'Python':
-            return str
-        else:
-            return(str[:n]+ str[n+1:])
+    if str == 'Python':
+        return str
+    else:
+        return(str[:n] + str[n + 1:])
