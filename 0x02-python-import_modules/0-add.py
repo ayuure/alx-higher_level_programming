89ded4239e6659ef2ab1cdb1effabfea22f7e0f5
ayuure <bertinaayuure0147@gmail.com>
2023-12-01 08:20:49 +0000

    first task

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" use import to print out values
+    format the int to strings
+    """
+import add_0
+a = 1
+b =2
+results = add_0.add(a,b)
+print("{} + {} = {}".format(a,b,results))
