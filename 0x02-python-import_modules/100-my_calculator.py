242ff73100431c35984fba75c7815366fa3f151b
ayuure <bertinaayuure0147@gmail.com>
2023-12-02 17:40:16 +0000

    fixed issues

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,16 +2,15 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, div, mul
     import sys
-    
 
     if len(sys.argv) -1 != 3:
         print('Usage: {} <a> <operator> <b>'.format(sys.argv[0]))
         sys.exit(1)
 
-    elif sys.argv[2] == "+" or sys.argv[2] == "-" or sys.argv[2] == "*" or sys.argv[2] == "/":
+    elif sys.argv[2] in {"+", "-", "*", "/"}:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-    
+
         if sys.argv[2] == "+":
             print('{} + {} = {}'.format(a, b, add(a,b)))
 
