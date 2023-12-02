939628726eff0ec70f098feeb7ae48d1fe3532cf
ayuure <bertinaayuure0147@gmail.com>
2023-12-02 18:35:33 +0000

    fixed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for names in dir(hidden_4):
-        if names[0] != '_' and names[1] != '_':
-            print(names)
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
