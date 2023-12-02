a49fe513c26dee40d96ce6b4bc7d7725de1730b7
ayuure <bertinaayuure0147@gmail.com>
2023-12-02 12:37:10 +0000

    second question and end added

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+import calculator_1 as calFunc
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, calFunc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calFunc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calFunc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calFunc.div(a, b)))
+
+
+
+
+
