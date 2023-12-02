ee2f03e10ae29580487bdeccdb878a8f7eeb2e6c
ayuure <bertinaayuure0147@gmail.com>
2023-12-02 12:23:09 +0000

    added if statement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,11 +2,9 @@
 """ use import to print out values
     format the int to strings
     """
-import add_0
+from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
 
-    results = add_0.add(a, b)
-
-    print("{} + {} = {}".format(a, b, results) + "\n")
+    print("{} + {} = {}".format(a, b, add(a, b)) + "\n")
