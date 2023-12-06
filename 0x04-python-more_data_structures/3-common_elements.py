05acaecc3d5d231ab4c9de922e0c8b9b08fa7405
ayuure <bertinaayuure0147@gmail.com>
2023-12-06 17:45:27 +0000

    3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                new_set = (i)
+    new_set = set_1.intersection(set_2)
     return new_set
