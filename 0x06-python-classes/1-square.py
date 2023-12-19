6e7be9282c530a4e12a2761945a9cce4031c3282
ayuure <bertinaayuure0147@gmail.com>
2023-12-19 19:32:41 +0000

    fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module provides utility functions for handling data.
+"""
 class Square:
+    """
+    A class for representing the calculation of area of a square.
+    """
     def __init__(self, size):
         self.__size = size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
+"""
+This module provides utility functions for handling data.
+"""
 class Square:
-    def __init__(self, size=0):
+    """
+    A class for representing the calculation of area of a square.
+    """
+    def __init__(self, size = 0):
         self.__size = size
+
+    @property    
     def size(self):
+        """Get the size of the square."""
         return self.__size
+    @size.setter
     def size(self, value):
+        """Set the size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
+        """Calculate the area of the square."""
         return self.__size * self.__size
     def my_print(self):
+        """Print the square."""
         if self.__size == 0:
             print()
-        theSq = "#" * self.__size
-        print(theSq)
-        print(theSq)
-
-
-my_square = Square(3)
-my_square.my_print()
-
-print("--")
-
-my_square.size = 10
-my_square.my_print()
-
-print("--")
-
-my_square.size = 0
-my_square.my_print()
-
-print("--")
+        theSq = "#" * (self.__size)
+        print("Size is: {}".format(self.__size))
+        for _ in range (self.__size):
+            print(theSq)
