3675fc8aa209e6069097f9ba6442986ded5dabfb
ayuure <bertinaayuure0147@gmail.com>
2023-12-19 19:46:17 +0000

    fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,10 +3,10 @@
 This module provides utility functions for handling data.
 """
 class Square:
-    def __init__(self, size = 0):
-        """
+    """
         A class for representing the calculation of area of a square.
         """
+    def __init__(self, size = 0):
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
