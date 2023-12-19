84e7067ebae62a2871a0b48d72b66a00cb20ecdd
ayuure <bertinaayuure0147@gmail.com>
2023-12-19 18:50:52 +0000

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+    def size(self):
+        return self.__size
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        return self.__size * self.__size
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        theSq = "#" * self.__size
+        print(theSq)
+        print(theSq)
+
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
