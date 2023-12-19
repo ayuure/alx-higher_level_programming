328b7a983f23d1ef33ea1bdafe0b357dbc1c33c2
ayuure <bertinaayuure0147@gmail.com>
2023-12-19 18:56:22 +0000

    added to one

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+This module provides utility functions for handling data.
+"""
 class Square:
+    """
+    A class for representing and manipulating data.
+    """
     pass
