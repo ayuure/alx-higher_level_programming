001c15a04d74a7cf43a476a7219f104c9e88505f
ayuure <bertinaayuure0147@gmail.com>
2024-01-02 16:08:16 +0000

    fixed

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     sum = 0
     try:
         sum = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         sum = None
     finally:
         print("Inside result: {:d}".format(sum))
