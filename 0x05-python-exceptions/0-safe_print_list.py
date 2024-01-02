0c60152aff243d1ff139b36a0e939a7e27f56e5b
ayuure <bertinaayuure0147@gmail.com>
2024-01-02 09:38:34 +0000

    fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,7 @@ def safe_print_list(my_list=[], x=0):
     length = 0
     i = 0
     num = 0
+    val = x - 1
 
     for vals in my_list:
         length += 1
@@ -13,11 +14,24 @@ def safe_print_list(my_list=[], x=0):
             x -= 1
             num += my_list[i]
             i += 1
-    except Exception:
+        print('{:d}'.format(num))
+        return my_list[val]
+    except IndexError:
         i = 0
         num = 0
         while i < length:
             num = num * 10
             num += my_list[i]
             i += 1
-    return num
+        print('{:d}'.format(num))
+        return my_list[-1]
+   
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
