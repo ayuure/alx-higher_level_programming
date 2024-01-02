b6de33322522446dd5a7a9b4493c20892e2951b5
ayuure <bertinaayuure0147@gmail.com>
2024-01-02 11:27:06 +0000

    fixed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    num = 0
-    length = 0
-    lenghtx = 0
-    xval = x
-    for val in my_list:
-        length += 1
-    try:
-        lenghtx = x
-        while x > 0:
-            x -= 1
-            num *= 10
-            num += my_list[i]
-            i += 1
-        print("{:d}".format(num))
-    except TypeError:
-        i = 0
-        num = 0
-        lenghtx = 0
-        while xval > 0:
-            if isinstance(my_list[i], int):
-                lenghtx += 1
-                num *= 10
-                num += my_list[i]
-            i += 1
-            xval -= 1
-        print("{}".format(num))
-    return lenghtx
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]), end='')
+        except (TypeError, ValueError):
+            pass
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
