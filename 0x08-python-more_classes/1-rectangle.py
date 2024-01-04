64379664ec6948e2710345beca510935e1bd4ae8
ayuure <bertinaayuure0147@gmail.com>
2024-01-04 21:22:14 +0000

    fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,14 +14,12 @@ class Rectangle:
     """
         self.__height = height
         self.__width = width
-    
     @property
     def width(self):
         """
         Getter for width
         """
         return(self.__width)
-    
     @width.setter
     def width(self, value):
         """
@@ -32,7 +30,6 @@ class Rectangle:
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
-
     @property
     def height(self):
         """
