b2cd777c605d156630dd62d20708ff7494ab70e1
ayuure <bertinaayuure0147@gmail.com>
2024-01-04 11:16:36 +0000

    first 2 questions

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" 
+    This is a class module
+"""
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+    This is a class module
+"""
+class Rectangle:
+    """
+    Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        self._width = width     
+        self._height = height  
+    @property
+    def width(self):
+        """
+        Getter for width
+        """
+        return(self._width)
+    @width.setter
+    def width(self, value):
+        """
+        Setter for width of rectangle
+        """
+        try:
+            if not isinstance(value, int):
+                raise TypeError('width must be an integer')
+            elif value < 0: 
+                raise ValueError('width must be >= 0')
+            else:
+                self._width = value
+        except (TypeError, ValueError)as e:
+            print(e)
+    @property
+    def height(self):
+        """
+        Getter for height of rectangle
+        """
+        return(self._width)
+    @height.setter
+    def height(self, value):
+        """
+        setter for height of rectangle
+        """
+        try:
+            if not isinstance(value, int):
+                raise TypeError('width must be an integer')
+            elif value < 0: 
+                raise ValueError('width must be >= 0')
+            else:
+                self._height = value
+        except (TypeError, ValueError) as e:
+            print(e)
