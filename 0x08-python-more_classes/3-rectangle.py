da1f423f4c991a55843883a3228f2442f924f0c0
ayuure <bertinaayuure0147@gmail.com>
2024-01-04 21:52:20 +0000

    done

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -2,6 +2,8 @@
 """
     This is a class module
 """
+
+
 class Rectangle:
     """
     Rectangle class
@@ -12,13 +14,14 @@ class Rectangle:
     """
         self.__height = height
         self.__width = width     
-          
+
     @property
     def width(self):
         """
         Getter for width
         """
         return(self.__width)
+
     @width.setter
     def width(self, value):
         """
@@ -26,15 +29,17 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        if value < 0: 
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
+
     @property
     def height(self):
         """
         Getter for height of rectangle
         """
         return(self.__height)
+
     @height.setter
     def height(self, value):
         """
@@ -42,14 +47,16 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        if value < 0: 
+        if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
+
     def area(self):
         """
         area of rectangle
         """
         return self.__height * self.__width
+
     def perimeter(self):
         """
         perimeter of rectangle
@@ -57,6 +64,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__height + self.__width)
+
     def print_str(self):
         """
         print of rectangle
@@ -67,9 +75,9 @@ class Rectangle:
         for i in range(self.__height):
             str_ += "#" * self.__width +'\n'
         return str_.rstrip('\n')
+
     def __str__(self):
         """
         print of rectangle
         """
         return self.print_str()
-    
