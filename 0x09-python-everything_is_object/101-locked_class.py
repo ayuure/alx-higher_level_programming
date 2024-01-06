fce6be9e3a5f830b86319628557f769f55379f57
ayuure <bertinaayuure0147@gmail.com>
2024-01-06 12:46:59 +0000

    done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Define a locked class"""
+
+
 class LockedClass():
     """
     Locked class that prevents the creation of other instance
     """
+    
     __slots__ = ["first_name"]
