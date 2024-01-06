cbb5de664dbfa48aa0e2210ac454546eae84dd65
ayuure <bertinaayuure0147@gmail.com>
2024-01-06 21:35:41 +0000

    c

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Add inter funtion that adds two float and or int values and returns the result
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')  
+    return a + b
