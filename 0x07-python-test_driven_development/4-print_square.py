b25ef39e1decb828e038d379fc8b5f33e477e0ce
ayuure <bertinaayuure0147@gmail.com>
2024-01-07 23:49:05 +0000

    done

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,10 +6,10 @@ print square
 
 def print_square(size):
     """print a square n number of times"""
-    if size < 0:
-        raise ValueError('size must be >= 0')
     if not isinstance(size, (int, float)):
         raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
     s = "#"
     for i in range(size):
         print(s * size)
