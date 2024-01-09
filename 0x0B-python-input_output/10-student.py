c50c01d942c19429c71ddc55dc3110a5c41688ab
ayuure <bertinaayuure0147@gmail.com>
2024-01-09 16:07:31 +0000

    done

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""This is a class"""
 class Student():
+    """class the converts to a dict"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """filter to make sure attr is in the class"""
         if attrs is None:
             return {
                 "first_name": self.first_name, 
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,9 +3,6 @@
 
 class Student():
     """Student class to is converted to json"""
-    first_name = ''
-    last_name = ''
-    age = 0
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
