dcb79c40895d5c719c4f2ef1052e31cb635309b5
ayuure <bertinaayuure0147@gmail.com>
2024-01-09 09:44:43 +0000

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""
+Read content from a file
+"""
 def read_file(filename=""):
-    with open(filename, 'r') as f:
+    """Function to read from a file"""
+    with open(filename, mode='r', encoding="utf-8") as f:
         f_content = f.read()
         print(f_content, end='')
