d7d8a2cbc9b4364da44fd30eea30bd1df6b3a1bc
ayuure <bertinaayuure0147@gmail.com>
2024-01-09 16:14:33 +0000

    f

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,6 +5,7 @@
 class Student():
     """class the converts to a dict"""
     def __init__(self, first_name, last_name, age):
+        """initilization function"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""This is a class"""
+"""
+This is a class
+"""
 
 
 class Student():
     """Student class to is converted to json"""
     def __init__(self, first_name, last_name, age):
+        """Initialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
