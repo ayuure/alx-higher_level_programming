8fac83a8df0a58ac5251c2b81c6689138869627a
ayuure <bertinaayuure0147@gmail.com>
2024-01-09 17:22:24 +0000

    d

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+This is a class
+"""
+
+
+class Student():
+    """
+    class the converts to a dict
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initilization function
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        filter to make sure attr is in the class
+        """
+
+        if attrs is None:
+            return self.__dict__
+        else:
+            return{attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """
+        changing the attr
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
