160c66f5bb4dd13b8c441907979610c745786d5d
ayuure <bertinaayuure0147@gmail.com>
2024-01-09 02:31:25 -0800

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,8 @@
 """
 Function to write to a file and return the number of charaters
 """
+
+
 def write_file(filename="", text=""):
     """Function used to write"""
     with open(filename, mode='w', encoding="utf-8") as f:
