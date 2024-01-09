a6eb2f138f5d485780ac193bdfde416fecea75f2
ayuure <bertinaayuure0147@gmail.com>
2024-01-09 10:57:45 +0000

    done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,5 +6,4 @@ def append_write(filename="", text=""):
     """Function to append text to a file"""
     with open(filename, mode='a', encoding='utf-8') as f:
         f.write(text)
-    with open(filename, mode='r', encoding='utf-8') as f:
-        return len(f.read())
+    return len(text)
