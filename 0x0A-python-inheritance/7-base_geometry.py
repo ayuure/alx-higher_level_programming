ebcdce2c4529f498ffd3755a656b4afa2b262a9c
ayuure <bertinaayuure0147@gmail.com>
2024-01-10 05:54:48 -0800

    F

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """This is a bass class"""
 
 
@@ -8,9 +7,10 @@ class BaseGeometry():
     def area(self):
         """area of baseGeo"""
         raise Exception('area() is not implemented')
+
     def integer_validator(self, name, value):
         """Validator"""
         if not isinstance(value, int):
             raise TypeError('{} must be an integer'.format(name))
-        if value <= 0: 
+        if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
