9900b3898323a0c3b96658bd6dd98bea6728daaa
ayuure <bertinaayuure0147@gmail.com>
2024-01-10 13:34:41 +0000

    done

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Function to print kind of class"""
 
+
+
 def is_kind_of_class(obj, a_class):
     """returns the kind of class and True/False"""
     return isinstance(obj, a_class)
