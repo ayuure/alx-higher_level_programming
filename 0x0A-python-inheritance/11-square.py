7f75069cc25523b3841e2a177506ddd33f68c916
ayuure <bertinaayuure0147@gmail.com>
2024-02-05 17:52:31 +0000

    done

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,8 +9,8 @@ class Square(Rectangle):
         """initializes size
         """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
 
     def __str__(self):
         """print out format"""
