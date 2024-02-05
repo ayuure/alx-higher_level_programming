033a5d064840782d81bf9f4f4dc9ae8868e19e1c
ayuure <bertinaayuure0147@gmail.com>
2024-02-05 07:45:54 -0800

    done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,6 @@
 """Function to check class and subclass"""
 
 
-
 def inherits_from(obj, a_class):
     """returns true if it is"""
     return type(obj) is not a_class and issubclass(type(obj), a_class)
