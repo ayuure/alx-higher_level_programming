5b639c736a769fd4f56ebe20b6e15828fb95c1c2
ayuure <bertinaayuure0147@gmail.com>
2024-02-05 18:08:20 +0000

    fixed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,3 +11,8 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def _str_(self):
+        """informal string reepresentation of the square"""
+        className = self._class.name_
+        return "{:s} {:d}/{:d}".format(className, self._size, self._size)
