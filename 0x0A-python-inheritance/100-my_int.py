abfee7b176832ac1d92d8d4cc5f3d7986d650d92
ayuure <bertinaayuure0147@gmail.com>
2024-02-05 17:16:35 +0000

    done

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,10 +3,15 @@
 
 
 class MyInt(int):
-
-    def __int__(self, value):
+    """+My int flips the logic of == and !="""
+    def __init__(self, value):
+        """ init method"""
         self.value =  value
+
     def __eq__(self, other):
+        """equal to method changed to !="""
         return self.value != other
+
     def __ne__(self, other):
+        """not equal to method shanged to =="""
         return self.value == other
