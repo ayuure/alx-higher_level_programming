076f3ad8a216e89b07852943851c81f02009d04b
Bertin-a <bertina.ayuure@amalitech.com>
2024-03-13 14:58:46 +0000

    fixed 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Prints the description of a table
-SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-AND table_name = 'first_table';
+-- Prints the full description of the table first_table.
+SHOW CREATE TABLE `first_table`;
