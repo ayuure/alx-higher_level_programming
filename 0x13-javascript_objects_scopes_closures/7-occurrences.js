e49b7f8d05c05202547bd6fc6369845de9967ef6
ayuure <bertinaayuure0147@gmail.com>
2024-04-09 17:30:02 +0000

    done

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let numOccurrences = 0;
-    for (let i = 0; i < list.length; i++) {
-      if (searchElement === list[i]) {
-        numOccurrences++;
-      }
+  let numOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      numOccurrences++;
     }
-    return numOccurrences;
-  };
+  }
+  return numOccurrences;
+};
