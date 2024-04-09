366be87c51b7fd407783245b6e0fd35843c27e75
Bertin-a <bertina.ayuure@amalitech.com>
2024-04-09 17:42:20 +0000

    done

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) || (h > 0)) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
@@ -15,6 +15,18 @@ class Rectangle {
       }
       console.log(c);
     }
+  }
+
+  rotate () {
+    let t;
+    t = this.height;
+    this.height = this.width;
+    this.width = t;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
   }
 }
 
