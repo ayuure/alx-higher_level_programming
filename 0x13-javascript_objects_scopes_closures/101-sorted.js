1e311b3fff7645d89aea90db8be64157b2fc03fd
ayuure <bertinaayuure0147@gmail.com>
2024-04-09 17:48:35 +0000

    d

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,9 +2,9 @@
 const { dict } = require('./101-data.js');
 const Dicn = {};
 for (const N in dict) {
-    if (Dicn[dict[N]] === undefined) {
-	Dicn[dict[N]] = [];
-    }
-    Dicn[dict[N]].push(N);
+  if (Dicn[dict[N]] === undefined) {
+    Dicn[dict[N]] = [];
+  }
+  Dicn[dict[N]].push(N);
 }
 console.log(Dicn);
