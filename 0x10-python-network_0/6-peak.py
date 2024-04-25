29efaaace70127455f94d0b05c8b8e83da5889a6
ayuure <bertinaayuure0147@gmail.com>
2024-04-25 17:59:18 +0000

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,19 +8,19 @@ def find_peak(list_of_integers):
         return None
 
     length = len(list_of_integers)
-    m = int(length / 2)
-    li = list_of_integers
+    ma = int(length / 2)
+    l = list_of_integers
 
-    if m - 1 < 0 and m + 1 >= length:
-        return li[m]
-    elif m - 1 < 0:
-        return li[m] if li[m] > li[m + 1] else li[m + 1]
-    elif m + 1 >= length:
-        return li[m] if li[m] > li[m - 1] else li[m - 1]
+    if ma - 1 < 0 and ma + 1 >= length:
+        return l[ma]
+    elif ma - 1 < 0:
+        return l[ma] if l[ma] > l[ma + 1] else l[ma + 1]
+    elif ma + 1 >= length:
+        return l[ma] if l[ma] > l[ma - 1] else l[ma - 1]
 
-    if li[m - 1] < li[m] > li[m + 1]:
-        return li[m]
+    if l[ma - 1] < l[ma] > l[ma + 1]:
+        return l[ma]
 
-    if li[m + 1] > li[m - 1]:
-        return find_peak(li[m:])
-    return find_peak(li[:m])
+    if l[ma + 1] > l[ma - 1]:
+        return find_peak(l[ma:])
+    return find_peak(l[:ma])
